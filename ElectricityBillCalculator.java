30152040734177a2b45145438e26e7b39ccc4d8f
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2025-07-13 20:45:20 +0530

    Update ElectricityBillCalculator.java

--- ElectricityBillCalculator.java
+++ ElectricityBillCalculator.java
@@ -6,12 +6,19 @@ public class ElectricityBillCalculator {
         System.out.print("Enter total units consumed: ");
         int units = scanner.nextInt();
         scanner.close();
-
         double bill = 0;
-
-        // TODO: Write the logic to calculate the bill based on units using
-        // if, if-else, and if-else-if statements only
-
+        if(units<=100){
+            bill=units*1.5;
+        }
+        else if(units>=101 && units<=300){
+            bill=units*2.5;
+        }
+        else if(units >=301 && units<=500){
+            bill=units*4;
+        }
+        else{
+            bill=units*6;
+        }
         System.out.println("Electricity Bill: ₹" + bill);
     }
 }
